a79fd8a528724471a5a8ac75e0835597ce6b724c
Robson Maia <robsonmvieira@gmail.com>
2022-10-17 16:41:43 -0300

    fix: :bug: fix import path from register style file

--- src/pages/Register/styles.ts
+++ src/pages/Register/styles.ts
@@ -1,5 +1,6 @@
-import { styled } from '../../src/stitches.config';
+// import { styled } from '../../src/stitches.config';
+import { styled } from '../../stitches.config'
 
 export const Container = styled('div', {
   // your styles here
-});
+})
